4f804ce9d6ae9e6374d599871bd8b67860e49bf0
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-02-04 15:57:09 -0300

    Main class refactored
    added the 'if __name__ == '__main__':'

--- main.py
+++ main.py
@@ -1,102 +1,109 @@
 import json
-import os
-
 from model import *
 
 
-def print_logo():
-    print(f"""
-┳┳┓┏┓┳┓┏┓┏┓  ┏┓┏┓┏┓┏┓┏┓┳┓┏┳┓┏┓┳┓
-┃┃┃┣┫┃┃┃┓┣┫  ┣  ┃┃ ┃┃┃┃┣┫ ┃ ┣ ┣┫
-┛ ┗┛┗┛┗┗┛┛┗  ┗┛┗┛┗┛┣┛┗┛┛┗ ┻ ┗┛┛┗
-                     {Colors.GRAY}for Houdoku{Colors.ENDC}
-""")
-
-
-def show_menu(series):
-    choice = ''
-    while choice != '0':
-        print(f"""    
-Options:
-
-{Colors.BOLD}1 - RESIZE THE COVERS{Colors.ENDC}
-    {Colors.GRAY}resizes the covers of each volume to the specified
-    size{Colors.ENDC}
-
-{Colors.BOLD}2 - EXPORT CHAPTERS TO PDF{Colors.ENDC}
-    {Colors.GRAY}collects  all  images from each chapter folder and
-    exports them to separate pdf files{Colors.ENDC}
-
-{Colors.BOLD}3 - EXPORT VOLUMES TO PDF{Colors.ENDC}
-    {Colors.GRAY}collects  all chapters from  each volume based on
-    the dictionary  and exports them to separate  pdf 
-    files{Colors.ENDC}
-    
-{Colors.BOLD}4 - EXPORT EVERYTHING{Colors.ENDC}
-    {Colors.GRAY}does both of the above: exports  the chapters then 
-    the volumes{Colors.ENDC}
-""")
-        choice = input("\n-> ")
-        if choice == '1':
-            series.resize_volume_covers()
-            Colors.job_done()
-        if choice == '2':
-            series.export_chapters()
-            Colors.job_done()
-        if choice == '3':
-            series.generate_volumes()
-            series.export_volumes()
-            Colors.job_done()
-        if choice == '4':
-            series.export_chapters()
-            series.generate_volumes()
-            series.export_volumes()
-            Colors.job_done()
-            choice = '0'
-
-
-def select_dictionary() -> object:
-    dictionary_list = [f for f in os.listdir('dictionaries') if f.endswith('.json')]
-
-    print(f"{Colors.BOLD}Select a dictionary:{Colors.ENDC}")
-    for i, dictionary in enumerate(dictionary_list):
-        print(f"{i + 1} - {dictionary}")
-    choice = input('\nType the number of the dictionary and press Enter: ')
-    with open(f'dictionaries\\{dictionary_list[int(choice) - 1]}', 'r') as config_file:
-        return json.load(config_file)
-
-
-def show_config(config):
-    print(f"""
-{Colors.BOLD}CONFIGURATION{Colors.ENDC}
-{Colors.BOLD}Name:{Colors.ENDC} {config["NAME"]}
-{Colors.BOLD}Author:{Colors.ENDC} {config["AUTHOR"]}
-{Colors.BOLD}Root folder:{Colors.ENDC} {config["ROOT"]}
-{Colors.BOLD}Volume filename template:{Colors.ENDC} {config["VOLUME_FILENAME_TEMPLATE"]}
-{Colors.BOLD}Cover size:{Colors.ENDC} {config["COVER_SIZE"]}
-{Colors.BOLD}Dictionary:{Colors.ENDC} {"Ok!" if config["DICTIONARY"] else "Not Ok!"}
-""")
-
-
 class Main:
-    print_logo()
-    config = select_dictionary()
 
-    NAME = config["NAME"]
-    AUTHOR = config["AUTHOR"]
-    ROOT = config["ROOT"]
-    VOLUME_FILENAME_TEMPLATE = config["VOLUME_FILENAME_TEMPLATE"]
-    COVER_SIZE = config["COVER_SIZE"]
-    DICTIONARY = config["DICTIONARY"]
+    @staticmethod
+    def print_logo():
+        """
+        Prints the logo of the program.
+        """
+        print(f"""
+    ┳┳┓┏┓┳┓┏┓┏┓  ┏┓┏┓┏┓┏┓┏┓┳┓┏┳┓┏┓┳┓
+    ┃┃┃┣┫┃┃┃┓┣┫  ┣  ┃┃ ┃┃┃┃┣┫ ┃ ┣ ┣┫
+    ┛ ┗┛┗┛┗┗┛┛┗  ┗┛┗┛┗┛┣┛┗┛┛┗ ┻ ┗┛┛┗
+                         {Colors.GRAY}for Houdoku{Colors.ENDC}
+    """)
 
-    show_config(config)
+    @staticmethod
+    def show_menu(series):
+        """
+        Shows the main menu and calls the appropriate method based on the user's choice.
+        :param series: the Series object
+        """
+        choice = ''
+        while choice != '0':
+            print(f"""    
+    Options:
+    
+    {Colors.BOLD}1 - RESIZE THE COVERS{Colors.ENDC}
+        {Colors.GRAY}resizes the covers of each volume to the specified
+        size{Colors.ENDC}
+    
+    {Colors.BOLD}2 - EXPORT CHAPTERS TO PDF{Colors.ENDC}
+        {Colors.GRAY}collects  all  images from each chapter folder and
+        exports them to separate pdf files{Colors.ENDC}
+    
+    {Colors.BOLD}3 - EXPORT VOLUMES TO PDF{Colors.ENDC}
+        {Colors.GRAY}collects  all chapters from  each volume based on
+        the dictionary  and exports them to separate  pdf 
+        files{Colors.ENDC}
+        
+    {Colors.BOLD}4 - EXPORT EVERYTHING{Colors.ENDC}
+        {Colors.GRAY}does both of the above: exports  the chapters then 
+        the volumes{Colors.ENDC}
+    """)
+            choice = input("\n-> ")
+            if choice == '1':
+                series.resize_volume_covers()
+                Colors.job_done()
+            if choice == '2':
+                series.export_chapters()
+                Colors.job_done()
+            if choice == '3':
+                series.generate_volumes()
+                series.export_volumes()
+                Colors.job_done()
+            if choice == '4':
+                series.export_chapters()
+                series.generate_volumes()
+                series.export_volumes()
+                Colors.job_done()
+                choice = '0'
+
+    @staticmethod
+    def select_dictionary() -> object:
+        """
+        Selects a dictionary from the dictionaries folder.
+        :return: json object
+        """
+        dictionary_list = [f for f in os.listdir('dictionaries') if f.endswith('.json')]
+
+        print(f"{Colors.BOLD}Select a dictionary:{Colors.ENDC}")
+        for i, dictionary in enumerate(dictionary_list):
+            print(f"{i + 1} - {dictionary}")
+        choice = input('\nType the number of the dictionary and press Enter: ')
+        with open(f'dictionaries\\{dictionary_list[int(choice) - 1]}', 'r') as config_file:
+            return json.load(config_file)
+
+    @staticmethod
+    def show_config(config):
+        """
+        Shows the configuration of the series.
+        :param config: json object
+        """
+        print(f"""
+    {Colors.BOLD}CONFIGURATION{Colors.ENDC}
+    {Colors.BOLD}Name:{Colors.ENDC} {config["NAME"]}
+    {Colors.BOLD}Author:{Colors.ENDC} {config["AUTHOR"]}
+    {Colors.BOLD}Root folder:{Colors.ENDC} {config["ROOT"]}
+    {Colors.BOLD}Volume filename template:{Colors.ENDC} {config["VOLUME_FILENAME_TEMPLATE"]}
+    {Colors.BOLD}Cover size:{Colors.ENDC} {config["COVER_SIZE"]}
+    {Colors.BOLD}Dictionary:{Colors.ENDC} {"Ok!" if config["DICTIONARY"] else "Not Ok!"}
+    """)
+
+
+if __name__ == '__main__':
+    Main.print_logo()
+    config = Main.select_dictionary()
+    Main.show_config(config)
     series: Series = Series(
-        name=NAME,
-        author=AUTHOR,
-        root=ROOT,
-        volumes_filename_template=VOLUME_FILENAME_TEMPLATE,
-        cover_size=COVER_SIZE,
-        dictionary=DICTIONARY,
+        name=config["NAME"],
+        author=config["AUTHOR"],
+        root=config["ROOT"],
+        volumes_filename_template=config["VOLUME_FILENAME_TEMPLATE"],
+        cover_size=config["COVER_SIZE"],
+        dictionary=config["DICTIONARY"],
     )
-
-    show_menu(series)
+    Main.show_menu(series)
